c0e02f1bb3b7b3e9994069684db06eff00e05d34
KalyaniSinghRajput11 <kalyanisinghrajput115@gmail.com>
2024-09-20 01:09:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  // can write any bullshit js code here
+  // always return html in a single entity
+  return (
+    <div>
+    <h1>Greetings</h1>
+  </div>
+  )
+}
+export default App
+
+// function call is repalced with function return value
